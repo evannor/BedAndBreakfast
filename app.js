48525addb66ffbc28246b34f14ddd3869280c377
evannor <erica.vannorman@gmail.com>
2021-01-17 14:54:18 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("Server is running on Port 3000");
+});
